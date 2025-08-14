05a7e76fe35934b9e45aaa8b587387fa53bafb7d
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-14 19:13:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'İşçiler / Road Workers',
+    title: 'Yol İşçileri / Road Workers',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     chapters: [
         {
             id: '1',
@@ -31,36 +31,6 @@ var config = {
             location: {
                 center: [28.95424276865587,41.01453943415163],
                 zoom: 13,
-                pitch: 60,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
-        {
-            id: '2',
-            alignment: 'left',
-            hidden: false,
-            title: '',
-            image: '',
-            description: '',
-            location: {
-                center: [28.95424276865587,41.01453943415163],
-                zoom: 17,
                 pitch: 60,
                 bearing: 0
             },
